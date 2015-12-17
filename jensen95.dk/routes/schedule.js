47af55b3e1a10d9e4d449d2f1bb4ab499c432a5a
Jensen95 <mj.95dk@gmail.com>
2015-12-17 04:35:54 +0100

    Added  eOddset and schedule pages and a Proc file for Heroku

---
+++ jensen95.dk/routes/schedule.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('schedule', { title: 'Schedule for NET' });
+});
+
+module.exports = router;

b7bafa62f3ccab07aa7638dbea4e35c64c3acfb0
Jensen95 <mj.95dk@gmail.com>
2015-12-17 05:04:00 +0100

    Added dynamic active highlight to the nav bar

--- jensen95.dk/routes/eoddset.js
+++ jensen95.dk/routes/eoddset.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('eoddset', { title: 'eOddset' });
+    res.render('eoddset', { title: 'eOddset', currentUrl: '/eoddset'});
 });
 
 module.exports = router;
--- jensen95.dk/routes/schedule.js
+++ jensen95.dk/routes/schedule.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('schedule', { title: 'Schedule for NET' });
+    res.render('schedule', { title: 'Schedule for NET', currentUrl: '/schedule'});
 });
 
 module.exports = router;

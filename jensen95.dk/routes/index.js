814b73db41f297063e6cdd93868e29d7c29f7477
Jensen95 <mj.95dk@gmail.com>
2016-01-18 09:58:05 +0100

    More testing

--- jensen95.dk/routes/index.js
+++ jensen95.dk/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express', currentUrl: '/'});
+    res.render('index', { title: 'Express', currentUrl: '/', breaking: 'Tester'});
 });
 
 module.exports = router;
